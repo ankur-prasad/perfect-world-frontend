ac801acae60892156d2f14982b066c3488a8630d
ankur-prasad <ankur.prasad@hotmail.com>
2025-12-02 12:13:57 +0100

    added home video

--- src/components/Home/MainVideo.tsx
+++ src/components/Home/MainVideo.tsx
@@ -21,13 +21,8 @@ export default function MainVideo() {
                 muted
                 loop
                 playsInline
-                src="https://assets.mixkit.co/videos/preview/mixkit-waves-in-the-water-1164-large.mp4"
+                src="/assets/videos/WEB PROMO FINAL.mp4"
             />
-            <div className="absolute inset-0 flex items-center justify-center z-20">
-                <h2 className="text-white text-5xl md:text-7xl font-bold uppercase tracking-tighter text-center px-4 font-primary">
-                    The World is Yours
-                </h2>
-            </div>
         </section>
     )
 }
